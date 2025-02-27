76870dce305d9dca48ca6091f0a1ea4de8dcaff0
Saksham-Shukla-05 <get467997@gmail.com>
2025-02-27 16:00:28 +0530

    Added Loading State

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,7 +9,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { login } from "@/http/api";
+import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 export function LoginForm() {
@@ -45,6 +47,7 @@ export function LoginForm() {
           <CardTitle className=" text-2xl">Login</CardTitle>
           <CardDescription>
             Enter your email below to login to your account
+            {mutation.isPending && <>Loading...</>}
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -81,8 +84,17 @@ export function LoginForm() {
                 onClick={handleLoginSubmit}
                 type="submit"
                 className="w-full"
+                disabled={mutation.isPending}
               >
-                Sign in
+                {mutation.isPending ? (
+                  <LoaderCircle
+                    className={cn(mutation.isPending ? "animate-spin" : "")}
+                  />
+                ) : (
+                  ""
+                )}
+
+                <span className="-mt-1">Sign in</span>
               </Button>
               <Button variant="outline" className="w-full">
                 Login with Google
